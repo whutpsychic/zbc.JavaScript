1391399b935657b0ace23c1454b83a3f01db2b36
zhangbc <zhangbc@mail.taiji.com.cn>
2018-06-07 13:22:14 +0800

    rr

---
+++ test1.js
@@ -0,0 +1,41 @@
+
+// ±Õ°ü
+function createFnc() {
+
+	var resault = [];
+
+	for (let i = 0; i < 10; i++) {
+		resault[i] = function () {
+			console.log(i);
+		}
+	}
+
+	return resault;
+}
+
+var arr = createFnc();
+console.log(arr);
+
+arr[0]()
+arr[1]()
+arr[2]()
+arr[3]()
+arr[4]()
+arr[5]()
+arr[6]()
+arr[7]()
+arr[8]()
+arr[9]()
+
+
+
+
+ 
+
+
+
+
+
+
+
+
