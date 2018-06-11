dc52e96e3c40af79c84d33c49eda4d16a0fc303c
zhangbc <zhangbc@mail.taiji.com.cn>
2018-06-11 11:36:52 +0800

    。。。。。

--- test1.js
+++ test1.js
@@ -1,36 +1,45 @@
 
-// 闭包
-function createFnc() {
+/*******************闭包***************************/
+//function createFnc() {
 
-	var resault = [];
+//	var resault = [];
 
-	for (let i = 0; i < 10; i++) {
-		resault[i] = function () {
-			console.log(i);
-		}
-	}
+//	for (let i = 0; i < 10; i++) {
+//		resault[i] = function () {
+//			console.log(i);
+//		}
+//	}
 
-	return resault;
-}
+//	return resault;
+//}
 
-var arr = createFnc();
-console.log(arr);
+//var arr = createFnc();
+//console.log(arr);
 
-arr[0]()
-arr[1]()
-arr[2]()
-arr[3]()
-arr[4]()
-arr[5]()
-arr[6]()
-arr[7]()
-arr[8]()
-arr[9]()
+//arr[0]()
+//arr[1]()
+//arr[2]()
+//arr[3]()
+//arr[4]()
+//arr[5]()
+//arr[6]()
+//arr[7]()
+//arr[8]()
+//arr[9]()
+
+/*******************闭包***************************/
+
+/******************* 比较 instanceof / typeof ***************************/
 
 
 
 
- 
+
+
+
+
+
+
 
 
 
