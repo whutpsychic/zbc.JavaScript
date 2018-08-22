45381b87534a748cf6866b2b200366786c31f3b0
zhangbc <zhangbc@mail.taiji.com.cn>
2018-08-22 16:13:49 +0800

    奥兰影城

---
+++ 奥兰环球影城偷鸡软件/tools.js
@@ -0,0 +1,28 @@
+﻿
+
+var tool = {};
+
+//将 xx:xx 格式的数据转换为 xxx 分钟 的函数
+tool.transform = function (str) {
+
+	if (typeof str !== "string") throw new Error("tool.transform 函数的传入参数不是一个字符串");
+
+	var _arr = str.split(":");
+
+	var _hour = parseInt(_arr[0]);
+	var _minutes = parseInt(_arr[1]);
+
+	console.log(_hour * 60 + _minutes, _hour, _minutes);
+	return _hour * 60 + _minutes;
+
+}
+
+
+
+
+
+
+
+
+
+
