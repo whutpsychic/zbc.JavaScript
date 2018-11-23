66b1180050d1b9af085c3fa95642cfed2deeb010
zhangbc <123456>
2018-11-23 16:18:37 +0800

    promise

--- ES6/es5/index.js
+++ ES6/es5/index.js
@@ -4,24 +4,40 @@
 // Promise.all
 // Promise.race
 var p1 = new Promise(function (resolve, reject) {
-  setTimout(function () {
+  setTimeout(function () {
     console.log("p1");
     resolve(40);
   }, 500);
 });
 var p2 = new Promise(function (resolve, reject) {
-  setTimout(function () {
+  setTimeout(function () {
     console.log("p2");
     resolve(41);
-  }, 8000);
+  }, 800);
 });
 var p3 = new Promise(function (resolve, reject) {
-  setTimout(function () {
+  setTimeout(function () {
     console.log("p3");
     resolve(42);
   }, 1000);
 });
-var p4 = Promise.all(p1, p2, p3);
-p1.then(function (data) {
+var p4 = Promise.race([p1, p2, p3]);
+var p5 = Promise.all([p1, p2, p3]);
+p4.then(function (data) {
   console.log(data);
+}).then(function () {
+  console.log(444);
 });
+p1.then(function () {
+  for (var i = 0; i < 10000; i++) {
+    console.log(i);
+  }
+}).then(function () {
+  console.log(22);
+}).then(function () {
+  console.log(33);
+}); //传入的promise对象组数据类型必须是数组
+//all 函数会等待所有 promise 都执行完才会调用其.then方法；其参数也是数组形式返回所有对应的resolve
+//race 函数会在第一个最先执行完promise之后立即调用其.then方法，并且不会阻止其余promise继续进程。
+//并且此时其参数为最快的那个promise的resolve
+//*race这里有个神奇的特性：就是执行次序，总是在最快的promise以及其第一个resolve都执行完之后，才会去执行race的.then；最快的promise如果后续还有.then则要排在之后
--- ES6/es6/Promise.js
+++ ES6/es6/Promise.js
@@ -33,6 +33,65 @@ promise2.then(null,(data)=>{
 
 
 
+//全局 promise 判断处理
+
+// Promise.all
+// Promise.race
+
+let p1 = new Promise((resolve,reject)=>{
+
+	setTimeout(()=>{
+		console.log("p1");
+		resolve(40)
+	},500)
+})
+
+let p2 = new Promise((resolve,reject)=>{
+
+	setTimeout(()=>{
+		console.log("p2");
+		resolve(41)
+	},800)
+})
+
+let p3 = new Promise((resolve,reject)=>{
+
+	setTimeout(()=>{
+		console.log("p3");
+		resolve(42)
+	},1000)
+})
+
+let p4 = Promise.race([p1,p2,p3]);
+let p5 = Promise.all([p1,p2,p3]);
+
+p4.then((data)=>{
+	console.log(data)
+}).then(()=>{
+	console.log(444)
+})
+
+p1.then(()=>{
+	for(let i=0;i<10000;i++){
+		console.log(i)
+	}
+}).then(()=>{
+	console.log(22)
+}).then(()=>{
+	console.log(33)
+})
+
+
+
+
+
+//传入的promise对象组数据类型必须是数组
+//all 函数会等待所有 promise 都执行完才会调用其.then方法；其参数也是数组形式返回所有对应的resolve
+//race 函数会在第一个最先执行完promise之后立即调用其.then方法，并且不会阻止其余promise继续进程。
+//并且此时其参数为最快的那个promise的resolve
+//*race这里有个神奇的特性：就是执行次序.
+//race的.then方法总是与最快的promise的.then方法对应进行（保持平行）并且滞后于同等级的最速promise
+
 
 
 
