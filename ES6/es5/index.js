28a46ea2dc5466c48213e956703d0a819d40c99d
b90_100007129645_zbc <b90_100007129645_zbc@git.cloud.tencent.com>
2018-11-25 10:22:54 +0800

    。。。

--- ES6/es5/index.js
+++ ES6/es5/index.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// copyWithin(startIndex*, copyItemStartIndex*, howmany)
-//用于将数组中的元素 fill 给自身
-//同 fill 皆用于定型数组之中
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-arr.copyWithin(2, 0);
-console.log(arr); //1212567890
-
-arr.copyWithin(3, 0, 2);
-console.log(arr); //1211267890
-
-arr.copyWithin(4, 2, 6);
-console.log(arr); //1211112678
-
-arr.copyWithin(2, 0, 5);
-console.log(arr); //1212111678
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
