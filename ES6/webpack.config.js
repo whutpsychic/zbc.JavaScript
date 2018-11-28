766b4705b0aba572889853603ae66d1cf90cb031
zhangbc <123456>
2018-11-28 13:08:00 +0800

    ES6 => es5兼容到ie9 环境已通

--- ES6/webpack.config.js
+++ ES6/webpack.config.js
@@ -2,9 +2,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './es5/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: ["@babel/polyfill",'./index.js'],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module:{
+		rules:[
+		{
+			test:/\.js$/,
+			exclude: __dirname + 'node_modules',
+			// include:__dirname + 'src',		//	目前可有可无
+			use:{
+				loader:'babel-loader',
+				options:{
+
+					// plugins:["@babel/plugin-transform-arrow-functions"]
+				}
+			}
+		}
+		]
+	}
 };
